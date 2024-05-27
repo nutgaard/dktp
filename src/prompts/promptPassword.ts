0d82e0bc0e9eee4ed83d8d0cab683131a856f4b1
Nicklas Utgaard <nutgaard@gmail.com>
2024-05-28 00:34:24 +0200

    fix: move inquirer stdio to stderr to avoid intermixing with ciphertext

--- src/prompts/promptPassword.ts
+++ src/prompts/promptPassword.ts
@@ -1,11 +1,11 @@
 import { Encryption, Password } from '../utils/encryption';
-import { DEBUG } from '../utils/targetPlatform';
 
 export async function promptPassword(): Promise<Password> {
-    if (DEBUG) return Encryption.asPassword('1234');
-
     const inquirer = await import('inquirer');
-    const response = await inquirer.default.prompt([
+    const prompt = inquirer.createPromptModule({
+        output: process.stderr,
+    });
+    const response = await prompt([
         {
             message: 'Password?',
             name: 'password',
