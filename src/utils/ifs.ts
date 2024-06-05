fe2505a60688419d15fdcd7a8e218bc2b0036fdf
Nicklas Utgaard <nutgaard@gmail.com>
2024-06-06 00:21:14 +0200

    feat(ifs): added support for ArrayBuffer, lastModified, existence of file, and deletion of file

--- src/utils/ifs.ts
+++ src/utils/ifs.ts
@@ -6,8 +6,11 @@ import { TARGET_PLATFORM, TargetPlatform } from './targetPlatform';
 type IFs = {
     name?: string;
     size: number;
+    lastModified: number;
+    arrayBuffer(): Promise<ArrayBuffer>;
     text(): Promise<string>;
     json(): Promise<any>;
+    exists(): Promise<boolean>;
 };
 
 export function getFS(type: TargetPlatform = TARGET_PLATFORM) {
@@ -15,11 +18,14 @@ export function getFS(type: TargetPlatform = TARGET_PLATFORM) {
         return {
             file: nodeRead,
             write: nodeWrite,
+            unlink: nodeUnlink,
         };
     } else {
         return {
             file: bunRead,
             write: bunWrite,
+            // Not implemented in bun; https://bun.sh/guides/write-file/unlink
+            unlink: nodeUnlink,
         };
     }
 }
@@ -28,15 +34,20 @@ function bunRead(path: string): IFs {
     // @ts-ignore
     return Bun.file(path);
 }
-function bunWrite(path: string, content: string): Promise<number> {
+function bunWrite(path: string, content: string | Buffer): Promise<number> {
     // @ts-ignore
     return Bun.write(path, content);
 }
 
 function nodeRead(path: string): IFs {
+    const stats = nodeStats(path);
     return {
         name: filePath.basename(path),
-        size: fsSync.statSync(path).size,
+        size: stats.size,
+        lastModified: stats.mtimeMs,
+        async arrayBuffer(): Promise<ArrayBuffer> {
+            return fs.readFile(path);
+        },
         async text(): Promise<string> {
             return fs.readFile(path, 'utf-8').then((it) => it.toString());
         },
@@ -44,10 +55,34 @@ function nodeRead(path: string): IFs {
             const content = await fs.readFile(path, 'utf-8').then((it) => it.toString());
             return JSON.parse(content);
         },
+        async exists(): Promise<boolean> {
+            return stats.exists;
+        },
     };
 }
-async function nodeWrite(path: string, content: string): Promise<number> {
+
+async function nodeWrite(path: string, content: string | Buffer): Promise<number> {
     await fs.mkdir(filePath.dirname(path), { recursive: true });
     await fs.writeFile(path, content, 'utf-8');
     return content.length;
+}
+
+function nodeUnlink(path: string): Promise<void> {
+    return fs.unlink(path);
+}
+
+function nodeStats(path: string) {
+    try {
+        const stats = fsSync.statSync(path);
+        return {
+            ...stats,
+            exists: true,
+        };
+    } catch {
+        return {
+            exists: false,
+            size: 0,
+            mtimeMs: 0,
+        };
+    }
 }
